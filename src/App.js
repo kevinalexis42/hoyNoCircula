2445e234f61dd6f6b31ba8e1c6597585f3a4242f
Alexis <ka51823@hotmail.com>
2024-10-16 14:42:10 -0500

    feature: agregacion estilos y mejoras visuales

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import RegistroAuto from './components/RegistroAuto';
+import ConsultaCirculacion from './components/ConsultaCirculacion';
+import './App.css';  
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app-container">
+        <nav className="navbar">
+          <ul className="navbar-list">
+            <li className="navbar-item">
+              <Link to="/registro" className="navbar-link">Registro de Auto</Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/consulta" className="navbar-link">Consulta de Circulación</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="content">
+          <Routes>
+            <Route path="/registro" element={<RegistroAuto />} />
+            <Route path="/consulta" element={<ConsultaCirculacion />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
